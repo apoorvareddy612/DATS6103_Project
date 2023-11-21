d51a153182b645723aaf7ece9fc6a9375c5a42d5
Apoorva Reddy <61325120+apoorvareddy612@users.noreply.github.com>
2023-11-21 09:31:28 -0500

    main code file

--- DATS6103.py
+++ DATS6103.py
@@ -1,3 +1,53 @@
 #%%
 #This is our main code file
+import pandas as pd
+import matplotlib.pyplot as plt 
+import seaborn as sns
 
+#%%
+data = pd.read_csv('cleaned_5250.csv')
+print(data.head())
+count_data = data.groupby(['discovery_year', 'detection_method']).size().unstack(fill_value=0)
+
+# Plot the stacked bar plot
+ax = count_data.plot(kind='bar', stacked=True, colormap='viridis')
+
+# Customize the plot
+ax.set_title('NASA Exoplanet Discovery Year and Detection Method')
+ax.set_xlabel('Discovery Year')
+ax.set_ylabel('Number of Exoplanets')
+ax.legend(title='Detection Method', bbox_to_anchor=(1.05, 1), loc='upper left')
+
+# Show the plot
+plt.show()
+
+count_data = data.groupby(['planet_type', 'detection_method']).size().unstack(fill_value=0)
+
+# Plot the stacked bar plot
+ax = count_data.plot(kind='bar', stacked=True, colormap='magma')
+
+# Customize the plot
+ax.set_title('NASA Exoplanet Discovery Year and Detection Method')
+ax.set_xlabel('Planet Type')
+ax.set_ylabel('Number of Exoplanets')
+ax.legend(title='Detection Method', bbox_to_anchor=(1.05, 1), loc='upper left')
+
+# Show the plot
+plt.show()
+
+#Orbital Period
+data['orbital_period'].value_counts()
+data['Orbital_Period'] = ''
+for i in range (len(data)):
+    if data['orbital_period'][i] < 1:
+        data['Orbital_Period'][i] = '<1'
+    elif data['Orbital_Period'][i] == 1:
+        data['Orbital_Period'][i] = '1'
+    else:
+        data['Orbital_Period'][i] = '>1'
+
+sns.boxplot(x='discovery_year', y='Orbital_Period', data=data)
+plt.title('Discovery Year vs Orbital Period of Exoplanets')
+plt.xlabel('Discovery Year')
+plt.ylabel('Orbital Period (years)')
+plt.show()
